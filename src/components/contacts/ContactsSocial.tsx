2d79f6fd8599d77caec45a22722582a64b421c01
denvysotskyi <denvysotskyi@gmail.com>
2021-11-25 18:55:38 +0200

    Modify styles of ContactsSocial

--- src/components/contacts/ContactsSocial.tsx
+++ src/components/contacts/ContactsSocial.tsx
@@ -4,10 +4,15 @@ import Image from '../../common/image/Image'
 import telegram from '../../assets/icons/social/telegram.svg'
 import linkedin from '../../assets/icons/social/linkedin.png'
 import github from '../../assets/icons/social/github.svg'
+import bitbucket from '../../assets/icons/social/bitbuck.svg'
 import phone from '../../assets/icons/social/phone.png'
 
 const LinksWrapper = styled.div`
+  width: 355px;
   margin-top: 20px;
+  @media ${props => props.theme.media.widescreen} {
+    min-width: 200px;
+  }
 `
 const Link = styled.a`
 `
@@ -15,7 +20,8 @@ const Link = styled.a`
 const ContactsSocial = (): JSX.Element => (
   <LinksWrapper>
     <Link href={'https://t.me/denvysotskyi'}
-          target={'_blank'}>
+          target={'_blank'}
+    >
       <Image src={telegram}
              alt={'telegram'}
              width={'45px'}
@@ -24,7 +30,8 @@ const ContactsSocial = (): JSX.Element => (
       />
     </Link>
     <Link href={'https://www.linkedin.com/in/denvysotskyi'}
-          target={'_blank'}>
+          target={'_blank'}
+    >
       <Image src={linkedin}
              alt={'linkedin'}
              width={'46px'}
@@ -33,9 +40,20 @@ const ContactsSocial = (): JSX.Element => (
       />
     </Link>
     <Link href={'https://github.com/denvysotskyi'}
-          target={'_blank'}>
+          target={'_blank'}
+    >
       <Image src={github}
              alt={'github'}
+             width={'45px'}
+             height={'45px'}
+             right={'25px'}
+      />
+    </Link>
+    <Link href={'https://bitbucket.org/denvysotskyi'}
+          target={'_blank'}
+    >
+      <Image src={bitbucket}
+             alt={'bitbucket'}
              width={'45px'}
              height={'45px'}
              right={'25px'}
